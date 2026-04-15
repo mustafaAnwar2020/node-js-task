3c7f3e4b4f353015c9221a62c8afd395e8770644
mustafaAnwar2020 <sasaxxx777@gmai.com>
2026-04-15 12:23:47 +0200

    git ignore

--- dist/utils/io.js
+++
@@ -1,8 +0,0 @@
-import { mkdir, writeFile } from "node:fs/promises";
-import path from "node:path";
-export async function writeJsonFile(outputDir, fileName, payload) {
-    await mkdir(outputDir, { recursive: true });
-    const filePath = path.join(outputDir, fileName);
-    await writeFile(filePath, JSON.stringify(payload, null, 2), "utf8");
-    return filePath;
-}
